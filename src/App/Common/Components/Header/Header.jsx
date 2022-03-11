7ba1199d5267e23a60dd75e9657243985347a13e
afdavletkulov <afdavletkulov@rttv.ru>
2022-03-11 22:58:24 +0300

    fix 2

--- src/App/Common/Components/Header/Header.jsx
+++ src/App/Common/Components/Header/Header.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import "./Header.scss";
-import logo from "../../assets/img/logo.png";
 
 export default function Header() {
   return (
     <div className="Header Header__root">
       <div className="App-content Header__box">
-        <div className="Header__logo">
+        <a href="" className="Header__logo">
           CODIT
-        </div>
+        </a>
         <nav className="Header__nav">
-          <div className="Header__nav-item">About me</div>
-          <div className="Header__nav-item">Portfolio</div>
+          <a href="" className="Header__nav-item">About me</a>
+          <a href="" className="Header__nav-item">Portfolio</a>
         </nav>
       </div>
     </div>
