24da1aedf33497aee84ac4f3f3e3c7f127fd7f30
afdavletkulov <afdavletkulov@rttv.ru>
2022-04-07 14:05:03 +0500

    fix

--- src/App/Blocks/Portfolio/Components/PortfolioBlock.jsx
+++ src/App/Blocks/Portfolio/Components/PortfolioBlock.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from "react";
+import "./styles/PortfolioBlock.scss";
+
 import Plashka from "../../../Common/Components/Plashka/Plashka";
 
-export default function PortfolioBlock({itemData}) {
- const {id, name, plashka} = itemData
- 
+export default function PortfolioBlock({ itemData }) {
+  const { id, name, plashka } = itemData;
+
   return (
     <div className="Portfolio__item">
       <div className="Portfolio__item-text">
         <h4 className="text">{name}</h4>
-        <Plashka data={plashka}/>
+        <Plashka data={plashka} />
       </div>
       <div className={`Portfolio__item-img Portfolio__item-img-${id}`}></div>
     </div>
-  )
+  );
 }
