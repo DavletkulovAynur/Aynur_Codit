de85fd608ff616a895a8960dc08221404d96b9b0
afdavletkulov <afdavletkulov@rttv.ru>
2022-05-02 21:36:23 +0500

    fix

--- src/App/Blocks/Portfolio/Components/Title.jsx
+++ src/App/Blocks/Portfolio/Components/Title.jsx
@@ -6,9 +6,6 @@ export default function Title() {
       <p className="Portfolio__title">
         I created STATAMIC, co-host FULL STACK RADIO, and am working on RADICAL
         DESIGN COURSE. I was homeschooled, live in Florida, wash dishes, shower
-        often, learn quickly, strive to put others first, and live my life for
-        Jesus. I'm a dad, husband, and a kid at heart. I wish I could
-        skateboard.
       </p>
     </>
   );
