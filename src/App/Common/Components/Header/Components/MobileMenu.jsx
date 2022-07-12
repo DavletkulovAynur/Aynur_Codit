62b19f13fbbde8fe93734efde8920610390ec3ac
afdavletkulov <afdavletkulov@rttv.ru>
2022-07-12 08:21:52 +0500

    fix

--- src/App/Common/Components/Header/Components/MobileMenu.jsx
+++ src/App/Common/Components/Header/Components/MobileMenu.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+import { HashLink as Link } from "react-router-hash-link";
 import "./styles/mobileMenu.scss";
 
 export default function MobileMenu({ closeMobileMenu }) {
+  const test = () => {
+    console.log('red')
+  }
   return (
     <div className="Mobile-menu">
       <div className="Mobile-menu__button-box" onClick={closeMobileMenu}>
         <button className="Mobile-menu__button">(close this thing)</button>
       </div>
       <div className="Mobile-menu__items">
-        <p className="Mobile-menu__item">About me</p>
-        <p className="Mobile-menu__item">Portfolio</p>
+        <Link onClick={closeMobileMenu} to="/#about-me" className="Mobile-menu__item">
+          About me
+        </Link>
+        <Link onClick={closeMobileMenu} to="/#portfolio" className="Mobile-menu__item">
+          Portfolio
+        </Link>
       </div>
     </div>
   );
